78460bcd37903239a6bdf3e00dd90b19ad582911
TheVoid-0 <marcodnmoura@gmail.com>
2022-06-23 20:10:32 -0300

    feat: avg by age

--- src/test-session/test-session.service.ts
+++ src/test-session/test-session.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { SessionAttempt } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 import { ShortlinkService } from 'src/shortlink/shortlink.service';
 import { UserService } from 'src/user/user.service';
@@ -31,13 +32,51 @@ export class TestSessionService {
   }
 
   async get(id: string) {
-    return this.prismaService.testSession.findUnique({
-      include: { owner: true, testAttempts: true },
+    const session = await this.prismaService.testSession.findUnique({
+      include: { owner: true, testAttempts: { include: { user: true } } },
       where: { id },
     });
+
+    if (!session) throw new NotFoundException();
+    const sessionResultsByAge = session.testAttempts.reduce((testsAcc, testAttempt) => {
+      const results = JSON.parse(testAttempt.results?.toString() ?? '{}') as Record<number, number>;
+      const {
+        user: { age },
+      } = testAttempt;
+      const resultsValues = Object.values(results);
+      const ageInUse = age ?? 0;
+
+      // Agrupa por idade e calcula a média das tentativas
+      if (!testsAcc[ageInUse]) {
+        testsAcc[ageInUse] = {
+          attempts: [{ ...testAttempt, avg: this.getAvg(Object.values(resultsValues)) }],
+          avg: 0,
+        };
+        return testsAcc;
+      }
+      testsAcc[ageInUse].attempts.push({ ...testAttempt, avg: this.getAvg(Object.values(resultsValues)) });
+
+      return testsAcc;
+    }, {} as Record<string, { attempts: Array<SessionAttempt & { avg: number }>; avg: number }>);
+
+    for (const results of Object.values(sessionResultsByAge)) {
+      results.avg = this.getAvg(results.attempts.map(attempt => attempt.avg));
+    }
+
+    return {
+      ...session,
+      testAttempts: sessionResultsByAge,
+    };
   }
 
   async getShortlink(id: string) {
     return this.shortlinkService.create(`${process.env.URL_WEB_TEST_SESSION}?sessionId=${id}`);
+  }
+
+  private getAvg(numbers: number[]): number {
+    return numbers.reduce((acc, val, i) => {
+      acc += val;
+      return i < numbers.length ? acc : acc / numbers.length;
+    }, 0);
   }
 }
